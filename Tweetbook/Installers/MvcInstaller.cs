8d98b541f394e83c5ae4b9f1a5d9c4a82131a0a9
K M <kmannopov@live.com>
2022-01-25 17:15:08 +0500

    Adding JWTs

--- Tweetbook/Installers/MvcInstaller.cs
+++ Tweetbook/Installers/MvcInstaller.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.OpenApi.Models;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 using Tweetbook.Options;
 
 namespace Tweetbook.Installers;
@@ -7,7 +10,44 @@ public class MvcInstaller : IInstaller
 {
     public void InstallServices(IServiceCollection services, IConfiguration configuration)
     {
-        services.AddSwaggerGen(x => { x.SwaggerDoc("v1", new OpenApiInfo { Title = "Tweetbook API", Version = "v1" }); });
+        var jwtSettings = new JwtSettings();
+        configuration.Bind(nameof(JwtSettings), jwtSettings);
+        services.AddSingleton(jwtSettings);
+
+        services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+            .AddJwtBearer(x =>
+            {
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSettings.Secret)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    RequireExpirationTime = false,
+                    ValidateLifetime = true
+                };
+            });
+
+        services.AddSwaggerGen(x => 
+        { 
+            x.SwaggerDoc("v1", new OpenApiInfo { Title = "Tweetbook API", Version = "v1" });
+
+            var security = new Dictionary<string, IEnumerable<string>>
+            {
+                {"Bearer", new string[0]}
+            };
+
+            x.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = "JWT"
+            });
+        });
         services.AddControllersWithViews();
         var swaggerOptions = new SwaggerOptions();
         configuration.GetSection(nameof(swaggerOptions)).Bind(swaggerOptions);
---
+++ Tweetbook/Options/JwtSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace Tweetbook.Options;
+
+public class JwtSettings
+{
+    public string Secret { get; set; }
+}
